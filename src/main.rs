069749191e0fce809bbd2063b7b7f4a4d037ccad
Gerard Glowacki <gerardrglowacki@gmail.com>
2022-08-29 22:33:47 +0900

    Co-authored-by: darida12321 <darida12321@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,21 @@ extern crate sdl2;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::gfx::primitives::DrawRenderer;
 use std::time::Duration;
 
+const SCREEN_WIDTH: u32 = 800;
+const SCREEN_HEIGHT: u32 = 600;
+
 pub fn main() -> Result<(), String> {
-    let sdl_context = sdl2::init()?;
-    let video_subsystem = sdl_context.video()?;
+    let sdl = sdl2::init()?;
+    let video_subsystem = sdl.video()?;
 
     let window = video_subsystem
-        .window("rust-sdl2 demo: Video", 800, 600)
+        .window("Thrust", SCREEN_WIDTH, SCREEN_HEIGHT)
         .position_centered()
+        .resizable()
         .opengl()
         .build()
         .map_err(|e| e.to_string())?;
@@ -21,7 +27,10 @@ pub fn main() -> Result<(), String> {
     canvas.set_draw_color(Color::RGB(255, 0, 0));
     canvas.clear();
     canvas.present();
-    let mut event_pump = sdl_context.event_pump()?;
+    let mut event_pump = sdl.event_pump()?;
+    let mut rect = Rect::new(SCREEN_WIDTH as i32 / 2 - 75, SCREEN_HEIGHT as i32 / 2 - 75, 150, 150);
+    let mut velocity = 5;
+
 
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -35,10 +44,21 @@ pub fn main() -> Result<(), String> {
             }
         }
 
+        canvas.set_draw_color(Color::RGB(255, 0, 0));
         canvas.clear();
+
+        canvas.set_draw_color(Color::RGB(0, 255, 0));
+        canvas.draw_rect(rect)?;
+        canvas.fill_rect(rect)?;
+
+        canvas.filled_circle(100, 100, 50, Color::BLUE);
         canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
-        // The rest of the game loop goes here...
+        std::thread::sleep(Duration::from_millis(1_000u64 / 30));
+        // The rest of the game loop goes here...   
+        rect.set_x(rect.x() + velocity);
+        if rect.x() < 0 || ((SCREEN_WIDTH - rect.width()) as i32) < rect.x() {
+            velocity *= -1;
+        }
     }
 
     Ok(())
