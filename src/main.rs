a084f6743f78e4301ec04d271094a064d9ff11e9
Gerard Glowacki <gerardrglowacki@gmail.com>
2022-08-29 22:53:37 +0900

    Add sdl2 features Make a moving circle Progress

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::gfx::primitives::DrawRenderer;
 use std::time::Duration;
+use std::collections::HashSet;
 
 const SCREEN_WIDTH: u32 = 800;
 const SCREEN_HEIGHT: u32 = 600;
@@ -31,6 +32,8 @@ pub fn main() -> Result<(), String> {
     let mut rect = Rect::new(SCREEN_WIDTH as i32 / 2 - 75, SCREEN_HEIGHT as i32 / 2 - 75, 150, 150);
     let mut velocity = 5;
 
+    let mut circle_x = 100;
+    let mut circle_y = 100;
 
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -44,6 +47,25 @@ pub fn main() -> Result<(), String> {
             }
         }
 
+        let keys = event_pump
+            .keyboard_state()
+            .pressed_scancodes()
+            .filter_map(Keycode::from_scancode)
+            .collect::<HashSet<_>>();
+        if keys.contains(&Keycode::Left) {
+            circle_x -= 5;
+        }
+        if keys.contains(&Keycode::Right) {
+            circle_x += 5;
+        }
+        if keys.contains(&Keycode::Up) {
+            circle_y -= 5;
+        }
+        if keys.contains(&Keycode::Down) {
+            circle_y += 5;
+        }
+        
+
         canvas.set_draw_color(Color::RGB(255, 0, 0));
         canvas.clear();
 
@@ -51,7 +73,7 @@ pub fn main() -> Result<(), String> {
         canvas.draw_rect(rect)?;
         canvas.fill_rect(rect)?;
 
-        canvas.filled_circle(100, 100, 50, Color::BLUE);
+        canvas.filled_circle(circle_x, circle_y, 50, Color::BLUE)?;
         canvas.present();
         std::thread::sleep(Duration::from_millis(1_000u64 / 30));
         // The rest of the game loop goes here...   
